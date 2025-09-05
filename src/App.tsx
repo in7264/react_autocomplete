541b6b62b4ebac7b059b113df33ec474c3fb0536
in7264 <in7264@gmail.com>
2025-09-05 15:09:29 +0300

    third

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
 import { DropdownItem } from './components/DropdownItem/DropdownItem';
-import debounce from 'lodash.debounce';
 import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const [dropdownInput, setDropdownInput] = useState('');
   const [currentPerson, setCurrentPerson] = useState<Person | null>(null);
 
-  const filteredPeople = useMemo(
-    () =>
-      peopleFromServer.filter(person =>
-        person.name.toLowerCase().includes(dropdownInput.toLowerCase()),
-      ),
-    [dropdownInput],
-  );
+  const handleSelect = (person: Person) => {
+    setCurrentPerson(person);
+  };
 
-  const [inputValue, setInputValue] = useState('');
-
-  const debouncedSetInput = useMemo(
-    () =>
-      debounce((value: string) => {
-        setDropdownInput(value);
-      }, 300),
-    [],
-  );
-
-  const showNoSuggestions = dropdownInput && filteredPeople.length === 0;
+  const handleInputChange = () => {
+    setCurrentPerson(null);
+  };
 
   return (
     <div className="container">
@@ -38,38 +24,12 @@ export const App: React.FC = () => {
             : 'No selected person'}
         </h1>
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              className="input"
-              data-cy="search-input"
-              value={inputValue}
-              onChange={event => {
-                setInputValue(event.target.value);
-                setCurrentPerson(null);
-                debouncedSetInput(event.target.value);
-              }}
-            />
-          </div>
-
-          <DropdownItem
-            peopleFromServer={filteredPeople}
-            setDropdownInput={setDropdownInput}
-            setCurrentPerson={setCurrentPerson}
-          />
-        </div>
-
-        {showNoSuggestions && (
-          <div
-            className="notification is-danger is-light mt-3"
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+        <DropdownItem
+          people={peopleFromServer}
+          onSelect={handleSelect}
+          onInputChange={handleInputChange}
+          debounceDelay={300}
+        />
       </main>
     </div>
   );
